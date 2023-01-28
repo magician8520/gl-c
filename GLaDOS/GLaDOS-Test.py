befbaee722cd7decaa5def1db29c9e3cc4759036
ChenAi <90126138+ChenAi140@users.noreply.github.com>
2023-01-28 15:05:27 +0800

    Update GLaDOS-Test.py

--- GLaDOS/GLaDOS-Test.py
+++ GLaDOS/GLaDOS-Test.py
@@ -113,8 +113,7 @@ def main():
         return
     
     for ck in cookies:
-        cookie = cookies.get(ck)
-        sign(cookie, pathd, date_time)
+        sign(ck, pathd, date_time)
    
     input('任意键结束：')
 
