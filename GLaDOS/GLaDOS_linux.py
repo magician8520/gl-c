a24414d5ee26e729c4dd8c1fdd1f4c7e8986b007
ChenAi <1403644796@qq.com>
2023-01-28 15:31:30 +0800

    222

--- GLaDOS/GLaDOS_linux.py
+++ GLaDOS/GLaDOS_linux.py
@@ -37,6 +37,7 @@ def start(cookie):
     if 'message' in checkin.text:
         mess = checkin.json()['message']
         time = state.json()['data']['leftDays']
+        print(state.json())
         days = time.split('.')[0]
         print(mess)
         print('剩余天数：' + days + '天')
@@ -46,49 +47,23 @@ def start(cookie):
 
     return checkin.status_code, state.status_code, mess, days
 
-def get_cks(pathd):
-    # 检查日志文件夹
-    if not path.exists(f'{pathd}/Clash_log'):
-        print('检测到没有日志文件夹，正则创建...')
-        mkdir(f'{pathd}/Clash_log')
-        print('日志文件夹创建成功！')
-
-    # 检查cookies配置
-    if not path.exists(f'{pathd}/cookies.yaml'):
-        print('检测到没有cookie.yaml文件，正在创建...\n')
-        file = open(f'{pathd}/cookies.yaml', 'w', encoding='utf-8')
-        file.write("# 一行一个cookie\n# 格式：\n# '账号': 'cookie'\n")
-        file.close()
-        print('cookies.yaml创建成功! \n')
-        print('你没有配置cookie，请将你的cookie按格式填入cookies.yaml文件中\n')
-        print('然后重新运行脚本')
-        return False
-
-    with open(f'{pathd}/cookies.yaml', 'r', encoding='utf-8') as fr:
-        cookies = yload(fr, yLoader)
-
-    if type(cookies) != dict:
-        print('请先配置cookie！！！')
-        return False
-
-    return cookies
 
 def sign(cookie, pathd):
     date_time = str(datetime.now())[:7]
-    try:
-        checkin_status_code, state_status_code, mess, days = start(cookie)
-        date_log = f'{str(datetime.now())[:19]}: checkin({checkin_status_code}); state({state_status_code}) \n    {mess} \n    剩余天数：{days}\n'
-        with open(f'{pathd}/Clash_log/Clash_{date_time}.log', 'a') as f:
-            f.write(date_log)
+    # try:
+    checkin_status_code, state_status_code, mess, days = start(cookie)
+    #     date_log = f'{str(datetime.now())[:19]}: checkin({checkin_status_code}); state({state_status_code}) \n    {mess} \n    剩余天数：{days}\n'
+    #     with open(f'{pathd}/Clash_log/Clash_{date_time}.log', 'a') as f:
+    #         f.write(date_log)
 
-    except Exception as e:
-        err_log = f'{datetime.now().date()}: {e}\n'
-        with open(f'{pathd}/Clash_log/Clash_{date_time}.log', 'a') as f:
-            f.write(err_log)
+    # except Exception as e:
+    #     err_log = f'{datetime.now().date()}: {e}\n'
+    #     with open(f'{pathd}/Clash_log/Clash_{date_time}.log', 'a') as f:
+    #         f.write(err_log)
             
-    finally:
-        with open(f'{pathd}/Clash_log/Clash_{date_time}.log', 'a') as f:
-            f.write('---===---===---===---===---===---===---\n')
+    # finally:
+    #     with open(f'{pathd}/Clash_log/Clash_{date_time}.log', 'a') as f:
+    #         f.write('---===---===---===---===---===---===---\n')
 
 def main():
     pathd = paths[0].replace('\\', '/')
