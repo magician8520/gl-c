e7f5b0785791af0a080cdc0c5cda1b89126e1b7e
ChenAi <1403644796@qq.com>
2023-02-11 15:35:45 +0800

    修改输出

--- GLaDOS/main.py
+++ GLaDOS/main.py
@@ -7,10 +7,12 @@ if __name__ == '__main__':
     SendKey = environ.get('SendKey')
     try:
         title, Text = CheckIn(ck)
+        print('签到成功！')
     except Exception as e:
-        title = '签到失败'
+        print('签到失败！')
+        title = '签到失败！'
         Text = e
     finally:
-        print(title)
+        # print(title)
         print(Text)
         send_msg(SendKey, title, Text)
