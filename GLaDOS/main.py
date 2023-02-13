6fd4e26a050888426aed4920a1b02555af334b82
ChenAi <1403644796@qq.com>
2023-02-13 12:08:14 +0800

    优化代码

--- GLaDOS/main.py
+++ GLaDOS/main.py
@@ -3,6 +3,7 @@ from Check import CheckIn
 from push import send_msg
 
 if __name__ == '__main__':
+    # 获取actions secrets配置的cookie SendKey
     ck = environ["cookie"]
     SendKey = environ.get('SendKey')
     try:
@@ -16,5 +17,5 @@ if __name__ == '__main__':
         # print(title)
         print(Text)
         # Text = Text.replace('\n', '%0D%0A%0D%0A')
-        Text = Text.replace('\n', '%0A%0A')
-        send_msg(SendKey, title, Text)
+        Text = Text.replace('\n', '%0A%0A')  # 替换\n，优化微信推送消息显示格式
+        send_msg(SendKey, title, Text)  # 推送消息，无SendKey不推送
--- GLaDOS/push.py
+++ GLaDOS/push.py
@@ -1,8 +1,10 @@
 from requests import post
 
+
 def send_msg(SendKey, title, Text):
-    
+
     if not SendKey:
+        # 无SendKey则拦截推送
         return False
     url = f'https://sctapi.ftqq.com/{SendKey}.send?title={title}&desp={Text}'
     post(url=url)
