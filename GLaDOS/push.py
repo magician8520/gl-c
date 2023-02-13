4dbba1f82f255a59b69923eb598a5fde218c891a
ChenAi <1403644796@qq.com>
2023-02-13 18:35:12 +0800

    优化消息推送代码

--- GLaDOS/push.py
+++ GLaDOS/push.py
@@ -13,12 +13,18 @@ def send_msg(SendKey, title, Text):
     pushid = rsp.json()['data']['pushid']
     readkey = rsp.json()['data']['readkey']
     state_url = f'https://sctapi.ftqq.com/push?id={pushid}&readkey={readkey}'
-    stop_time = datetime.now() + timedelta(minutes=1, seconds=0)
+    stop_time = datetime.now() + timedelta(minutes=0, seconds=30)
+    count = 1
     while True:
         status_rsp = get(url=state_url)
         result = status_rsp.json()['data']['wxstatus']
+        now_time = datetime.now()
+        print(now_time, ' ---> ', stop_time, '  :  ', count)
         if result:
             return '消息推送成功！'
-        elif datetime.now() >= stop_time:
+        elif now_time >= stop_time:
             return '程序运行结束！推送结果未知！'
+        elif count >= 60:   # 防止程序一直运行
+            return '程序运行结束！推送结果未知！'
+        count += 1
         sleep(1)
