f3cdd5dd9544f7a5dbede7f59edd1e04cac37b0f
ChenAi <1403644796@qq.com>
2023-02-13 18:01:46 +0800

    更新

--- GLaDOS/push.py
+++ GLaDOS/push.py
@@ -6,17 +6,17 @@ def send_msg(SendKey, title, Text):
 
     if not SendKey:
         # 无SendKey则拦截推送
-        return '未配置SendKey，无法进行微信消息推送。'
+        return '未配置SendKey，无法进行消息推送。'
     url = f'https://sctapi.ftqq.com/{SendKey}.send?title={title}&desp={Text}'
     rsp = post(url=url)
     pushid = rsp.json()['data']['pushid']
     readkey = rsp.json()['data']['readkey']
-    status_url = f'https://sctapi.ftqq.com/push?id={pushid}&readkey={readkey}'
+    state_url = f'https://sctapi.ftqq.com/push?id={pushid}&readkey={readkey}'
     stop_time = datetime.now() + timedelta(minutes=1, seconds=0)
     while True:
-        status_rsp = get(url=status_url)
+        status_rsp = get(url=state_url)
         result = status_rsp.json()['data']['wxstatus']
         if result:
             return '消息推送成功！'
         elif datetime.now() >= stop_time:
-            return '程序运行结束！推送未成功！'
+            return '程序运行结束！推送结果未知！'
