4bf39cb9c12ad3d6d072e827deaee2c828454bbe
ChenAi <1403644796@qq.com>
2023-02-13 17:57:16 +0800

    优化消息推送

--- GLaDOS/main.py
+++ GLaDOS/main.py
@@ -10,12 +10,13 @@ if __name__ == '__main__':
         title, Text = CheckIn(ck)
         print('签到成功！')
     except Exception as e:
-        print('签到失败！')
-        title = '签到失败！'
+        print('程序出错！')
+        title = '程序出错！'
         Text = e
     finally:
         # print(title)
         print(Text)
         # Text = Text.replace('\n', '%0D%0A%0D%0A')
         Text = Text.replace('\n', '%0A%0A')  # 替换\n，优化微信推送消息显示格式
-        send_msg(SendKey, title, Text)  # 推送消息，无SendKey不推送
+        rsp = send_msg(SendKey, title, Text)  # 推送消息，无SendKey不推送
+        print(rsp)
--- GLaDOS/push.py
+++ GLaDOS/push.py
@@ -1,4 +1,5 @@
-from requests import post
+from requests import post, get
+from datetime import datetime, timedelta
 
 
 def send_msg(SendKey, title, Text):
@@ -7,4 +8,15 @@ def send_msg(SendKey, title, Text):
         # 无SendKey则拦截推送
         return False
     url = f'https://sctapi.ftqq.com/{SendKey}.send?title={title}&desp={Text}'
-    post(url=url)
+    rsp = post(url=url)
+    pushid = rsp.json()['data']['pushid']
+    readkey = rsp.json()['data']['readkey']
+    status_url = f'https://sctapi.ftqq.com/push?id={pushid}&readkey={readkey}'
+    stop_time = datetime.now() + timedelta(minutes=1, seconds=0)
+    while True:
+        status_rsp = get(url=status_url)
+        result = status_rsp.json()['data']['wxstatus']
+        if result:
+            return '消息推送成功！'
+        elif datetime.now() >= stop_time:
+            return '程序运行结束！推送未成功！'
